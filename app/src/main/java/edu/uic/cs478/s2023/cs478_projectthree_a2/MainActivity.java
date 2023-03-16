8efa1b3024aadbbfb5f8d72695b59ccf48386c25
GGMachina <ggonza55@uic.edu>
2023-03-15 20:29:13 -0500

    Now fully capable of receiving a broadcast from A1.

--- app/src/main/java/edu/uic/cs478/s2023/cs478_projectthree_a2/MainActivity.java
+++ app/src/main/java/edu/uic/cs478/s2023/cs478_projectthree_a2/MainActivity.java
@@ -9,37 +9,32 @@ import android.content.IntentFilter;
 import android.os.Bundle;
 import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
-    BroadcastReceiver receiver;
     final String PROJECT_PERM = "edu.uic.cs478.spring23.mp3";
+    BroadcastReceiver pReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
-
-        IntentFilter receiverFilter = new IntentFilter();
-        receiverFilter.addAction(PROJECT_PERM);
-
-        receiver = new BroadcastReceiver() {
+    @Override
+    protected void onStart() {
+        super.onStart();
+        IntentFilter receiverFilter = new IntentFilter(PROJECT_PERM);
+        pReceiver = new BroadcastReceiver() {
             @Override
             public void onReceive(Context context, Intent intent) {
-//                TODO: Check if who we received has the right permission
                 String chosenCity = intent.getStringExtra("sentCity");
                 Toast.makeText(context, "Received Broadcast: " + chosenCity, Toast.LENGTH_SHORT).show();
             }
         };
-
-        registerReceiver(receiver, receiverFilter);
-
+        registerReceiver(pReceiver, receiverFilter);
     }
 
     @Override
-    protected void onDestroy() {
-        if (receiver != null) {
-            unregisterReceiver(receiver);
-            receiver = null;
-        }
-        super.onDestroy();
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(pReceiver);
     }
 }
